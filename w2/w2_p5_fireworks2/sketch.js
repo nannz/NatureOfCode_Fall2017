c0bc59d5f3dbb4b70e8c3a0377bf0d437d9abfc7
Nan Zhao <nanzhao@nyu.edu>
2017-09-18 14:12:10 +0800

    Thank you message & some notes
    I lost my notes in the code!!! Anyway, I typed them again…

--- w2/w2_p5_fireworks2/sketch.js
+++ w2/w2_p5_fireworks2/sketch.js
@@ -1,16 +1,39 @@
 "use strict";
 
+//vector using for better arrangement: velocity, position, accelerometer
+//high saturation color change while triggering dragging (HSB colormode)
+//lifeSpan setting and thank you message.
+//different modes of fireworks. 1.explode, rotate, and die 2.stop dying and follow the mouse
+//instruction & fading effect.
+
+//problem met
+//1. the dia can be negative. Solved it by adding check() after update() & add isDead boolean
+//2. set a mode to check which mode/state the fireworks are in
+//3. had spent some time on figuring out what data needs to be updated whenever mouseDragged/Pressed/Clicked/Released
+
+//also tried:WEBGL, read some examples on presenting it on mobile. See my github link w2 with all the inclass practices and personal tries
+//https://github.com/nannz/NatureOfCode_Fall2017/tree/master/w2
+
+//next:
+//1.presenting it on mobile with acceleration, maybe the color of the background can change
+//2.let the objects move in the direction of the shape from a svg file. maybe I can start with vertex.
+
+
+
 
 var circles = [];
 var numOfFire = 200;
 var mode;
+var instruction;
+var insColor;
 
 function setup() {
   createCanvas(600, 600);
   background(0);
   colorMode(HSB, 255);
   mode = 0;
-
+  insColor = 255;
+  instruction = "try keypressed, mouseDragged, mouseClicked";
   for (var i = 0; i < numOfFire; i++) {
     circles[i] = new Circle(width / 2, height, -10);
   }
@@ -18,16 +41,34 @@ function setup() {
 }
 
 function draw() {
-  background(0,10);
+  background(0, 10);
+  //instruction
+  push();
+  fill(insColor);
+  text(instruction, 10, 10);
+  insColor -= 1;
+  if (insColor == 0) {
+    insColor = 0;
+  }
+  pop();
+  
   for (var i = 0; i < circles.length; i++) {
     var c = circles[i];
     c.update(mode);
     c.check();
     c.display(mode);
-    
-    if(c.isDead){
-      circles.splice(i,1);
+
+    if (c.isDead) {
+      circles.splice(i, 1);
     }
+  }
+  
+  if(circles.length == 0){
+    push();
+    fill(255);
+    textAlign(CENTER);
+    text("Wish you have fun. Thank you. :)", width/2, height/2);
+    print("all died!");
   }
 }
 
@@ -48,99 +89,18 @@ function mouseDragged() {
 
 function mouseReleased() {
   mode = 0;
-   for (var i = 0; i < circles.length; i++) {
+  for (var i = 0; i < circles.length; i++) {
     var c = circles[i];
     c.count = 0;
     c.isExploded = true;
     c.explode();
   }
 }
-function mousePressed(){
-  var newColor = color(random(255),random(0,100),255); 
+
+function mousePressed() {
+  var newColor = color(random(255), random(0, 100), 255);
   for (var i = 0; i < circles.length; i++) {
     var c = circles[i];
     c.circleFill = newColor;
-  }
-}
-
-
-class Circle {
-
-  constructor(_x, _y, _velY) {
-    this.position = createVector(_x, _y);
-    this.acc = createVector(0, 0);
-    this.vel = createVector(0, _velY);
-
-    this.isExploded = false;
-    this.isDead = false;
-    
-    this.angle = random(PI * 2);
-    this.angleVel = random(0.05, 0.01);
-    this.count = 0;
-
-    this.dia = random(5.0, 10.0);
-    this.circleFill = color(0,0,255); //hsb
-    this.trans = 255;
-  }
-  update(mode) {
-    
-    this.updateAcc(mode);
-    this.vel.add(this.acc);
-    this.position.add(this.vel);
-
-    if (this.isExploded) {
-      this.vel.mult(0.9);
-      this.dia = this.dia - 0.01;
-      this.count++;
-    }
-    // if (this.isExploded && mode == 1){
-    //   this.dia = this.dia - 0.01;
-    // }
-    //this.trans = this.trans -1;
-  }
-  
-  updateAcc(mode) {
-    if (mode == 0) {
-      this.acc = createVector(0, 0.1);
-    } else if (mode == 1) {
-      //mouse
-      var mouseVector = createVector(mouseX, mouseY);
-      var dir = mouseVector.sub(this.position);
-      dir.normalize();
-      dir.mult(0.5);
-      this.acc = dir;
-    }
-  }
-  explode() {
-    this.isExploded = true;
-    this.vel.x = random(-10, 10);
-    this.vel.y = random(-10, 10);
-  }
-  check(){
-    if(this.trans == 0){
-      this.isDead = true;
-    }
-    if(this.dia == 0.1){
-      this.isDead = true; 
-      print("dia = 0.1");
-      this.dia = 0.1;
-    }
-    
-  }
-  display(mode) {
-    if (mode == 0) {
-      push();
-      translate(this.position.x, this.position.y);
-      rotate(frameCount * this.angleVel + this.angle);
-      noStroke();
-      fill(this.circleFill);
-       ellipse(this.count, 0, this.dia, this.dia);
-      //ellipse(this.count, 0, -10,-10);
-      pop();
-    } else if (mode == 1) {
-      noStroke();
-      fill(this.circleFill);
-      ellipse(this.position.x, this.position.y, this.dia, this.dia);
-    }
   }
 }
