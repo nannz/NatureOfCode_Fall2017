130b5d42011f7cf45d09bb01a29cdcfdf2a6cf7b
Nan Zhao <nanzhao@nyu.edu>
2017-09-21 14:30:09 +0800

    Vector calculation in force
    Be careful about the angle calculation of vector

--- w3/inClass_force/sketch.js
+++ w3/inClass_force/sketch.js
@@ -1,24 +1,79 @@
 "use strict";
-
+//this.vel.heading() function //Calculate the angle of rotation for this vector (only 2D vectors)
+//force = p5.Vector.fromAngle(angle - PI / 2);
+var c;
 
 function setup() {
-  createCanvas(600,600);
-}
-
-function draw() {
+  createCanvas(600, 600);
+  c = new Circle(width / 2, height / 2);
   background(0);
 }
 
-class Circle {
-  constructor(){
-    this.pos = createVector();
-    this.vel = createVector();
-    this.acc = createVector();
+function draw() {
+  //background(0);
+
+  var angle = c.vel.heading();
+  print(angle);
+
+  if (keyIsPressed) {
+    var force = createVector();
+    if (keyCode == LEFT_ARROW) {
+      force = p5.Vector.fromAngle(angle - PI / 2);
+    } else if (keyCode == RIGHT_ARROW) {
+      force = p5.Vector.fromAngle(angle + PI / 2);
+    }
+
+    force.mult(0.1);
+    c.applyForce(force);
   }
-  
-  update(){
+
+
+
+  c.update();
+  c.bounce();
+  c.display();
+}
+
+class Circle {
+  constructor(_x, _y) {
+    this.pos = createVector(_x, _y);
+    this.vel = createVector(1, 0);
+    this.acc = createVector();
+    this.r = 50;
+  }
+
+  update() {
     this.vel.add(this.acc);
     this.pos.add(this.vel);
-    this.acc.mult(0);
+    this.acc.mult(0); //reset the acc
+  }
+
+  applyForce(force) {
+    this.acc = force;
+  }
+
+  //checkEdge
+  bounce() {
+    if (this.pos.x < 0 || this.pos.x > width) {
+      this.vel.x = -this.vel.x;
+    }
+    if (this.pos.y < 0 || this.pos.y > height) {
+      this.vel.y = -this.vel.y;
+    }
+    this.pos.x = constrain(this.pos.x, 0, width);
+    this.pos.y = constrain(this.pos.y, 0, height);
+  }
+
+  display() {
+    push();
+    translate(this.pos.x, this.pos.y);
+
+    noStroke();
+    fill(255);
+    //ellipse(0, 0, this.r, this.r);
+
+    stroke(255,0,0);
+    line(0,0,this.vel.x * 10, this.vel.y*10);
+    pop();
   }
 }
