0e440af20807300da4336c85cc7b691f684b9072
Nan Zhao <nanzhao@nyu.edu>
2017-09-21 15:06:27 +0800

    Careful about gravity*mass

--- w3/inClass_force/sketch.js
+++ w3/inClass_force/sketch.js
@@ -1,19 +1,43 @@
 "use strict";
+//be careful about the vector from angle.
 //this.vel.heading() function //Calculate the angle of rotation for this vector (only 2D vectors)
 //force = p5.Vector.fromAngle(angle - PI / 2);
+// acc = force/mass
+//careful to calculate gravity. should multi mass
+
 var c;
+var circles = [];
 
 function setup() {
   createCanvas(600, 600);
-  c = new Circle(width / 2, height / 2);
+  //c = new Circle(width / 2, height / 2);
+
+  circles[0] = new Circle(width / 2 - 150, height / 2);
+  circles[1] = new Circle(width / 2, height / 2);
+  circles[2] = new Circle(width / 2 + 150, height / 2);
   background(0);
 }
 
 function draw() {
-  //background(0);
+  background(0);
+  for (var i = 0; i < circles.length; i++) {
+    var c = circles[i];
+    var gravity = createVector(0, 1);
+    gravity.mult(c.mass);
+    c.applyForce(gravity);
+    
+    var wind = createVector(1,0);
+    c.applyForce(wind);
 
+    c.update();
+    c.bounce();
+    c.display();
+
+  }
+
+/*
   var angle = c.vel.heading();
-  print(angle);
+  //print(angle);
 
   if (keyIsPressed) {
     var force = createVector();
@@ -27,19 +51,21 @@ function draw() {
     c.applyForce(force);
   }
 
+*/
 
 
-  c.update();
-  c.bounce();
-  c.display();
 }
 
 class Circle {
   constructor(_x, _y) {
     this.pos = createVector(_x, _y);
-    this.vel = createVector(1, 0);
+    this.vel = createVector(0, 0);
     this.acc = createVector();
-    this.r = 50;
+    this.mass = random(1, 10);
+    this.r = map(this.mass,1,10,10,50);
+
+    //*** set mass from 1
+    
   }
 
   update() {
@@ -48,17 +74,20 @@ class Circle {
     this.acc.mult(0); //reset the acc
   }
 
-  applyForce(force) {
-    this.acc = force;
+  applyForce(force) { 
+    force.div(this.mass);
+    this.acc.add(force);
   }
 
   //checkEdge
   bounce() {
     if (this.pos.x < 0 || this.pos.x > width) {
       this.vel.x = -this.vel.x;
+      this.vel.mult(0.8);
     }
     if (this.pos.y < 0 || this.pos.y > height) {
       this.vel.y = -this.vel.y;
+      this.vel.mult(0.8);
     }
     this.pos.x = constrain(this.pos.x, 0, width);
     this.pos.y = constrain(this.pos.y, 0, height);
@@ -70,10 +99,10 @@ class Circle {
 
     noStroke();
     fill(255);
-    //ellipse(0, 0, this.r, this.r);
+    ellipse(0, 0, this.r, this.r);
 
-    stroke(255,0,0);
-    line(0,0,this.vel.x * 10, this.vel.y*10);
+    stroke(255, 0, 0);
+    line(0, 0, this.vel.x * 10, this.vel.y * 10);
     pop();
   }
 }
